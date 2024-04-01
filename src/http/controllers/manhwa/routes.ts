2274ba592424422f8192982bac0208310d8c5dc3
Marcos Zaghetto <marcoszaghetto@Marcoss-MacBook-Pro.local>
2024-03-31 22:14:33 -0300

    feat: verifyUserRole to update a manhwa

--- src/http/controllers/manhwa/routes.ts
+++ src/http/controllers/manhwa/routes.ts
@@ -2,13 +2,14 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
 import { createManhwa } from './create-manhwa'
 import { updateManhwa } from './update-manhwa'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 export async function manhwaRoutes(app: FastifyInstance) {
   app.post('/manhwa/create', { onRequest: [verifyJWT] }, createManhwa)
 
   app.patch(
     '/manhwa/:manhwaID/update',
-    { onRequest: [verifyJWT] },
+    { onRequest: [verifyJWT, verifyUserRole('admin')] },
     updateManhwa,
   )
 }
